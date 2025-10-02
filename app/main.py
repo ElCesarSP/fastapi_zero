d6b9cedafb3671abed5dfc0391aa201e773e629f
ElCesarSP <cs.loko0125@gmail.com>
2025-10-02 09:22:28 -0300

    feact: schemas.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from http import HTTPStatus
+from app.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code= HTTPStatus.OK, response_model= Message )
 def root():
     return {'message': 'Hello World'}
+
+
+@app.post('/Creat')
+def creat():
+    return{ 'message': 'Create funcionario'}
